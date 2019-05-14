c0c7a2193ba1659fa5f81162fcd74301f84d7d97
LRS4 <42714431+LRS4@users.noreply.github.com>
2019-05-14 20:51:16 +0100

    Create mario.c

---
+++ Pset1/mario.c
@@ -0,0 +1,32 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // get height of pyramid
+    int height;
+    do
+    {
+        height = get_int("Height: ");
+    }
+    while (height < 1 || height > 8);
+    
+    // for (initialization; condition; update) i++ (equal to i = i + 1)
+    for (int i = 0; i < height; i++)
+    {
+        // print spaces
+        for (int spaces = (height -(i)) ; spaces > 1; spaces--)
+        {
+            printf(" ");    
+        }
+        
+        // print hashes
+        for (int hashes = 0; hashes < (i + 1); hashes++)
+        {
+            printf("#");    
+        }
+        
+        // print new line
+        printf("\n");
+    }
+}
