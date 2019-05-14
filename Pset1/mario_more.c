7c3638c438075c57ac5054cf7b21f783a4023731
LRS4 <42714431+LRS4@users.noreply.github.com>
2019-05-14 20:51:19 +0100

    Create mario_more.c

---
+++ Pset1/mario_more.c
@@ -0,0 +1,39 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int height;
+    
+    do
+    {
+        height = get_int("Height: ");
+    }
+    while (height < 1 || height > 8);
+    
+    for (int i = 0; i < height; i++)
+    {
+        // loop for spaces
+        for (int spaces = height - 1; spaces > i; spaces--)
+        {
+            printf(" ");
+        }
+        
+        // loop for first set of hashes
+        for (int hashes = 1; hashes < i + 1; hashes++)
+        {
+            printf("#");
+        }
+        
+      printf("#  #");
+        
+        // loop for second set of hashes
+        for (int hashes = 1; hashes < i + 1; hashes++)
+        {
+            printf("#");
+        }
+        
+      printf("\n");
+        
+    }
+}
